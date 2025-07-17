cf2d6ad2611e6d1dd559bbb47994e45f5e5aa7b4
FlockEagle <ecgeodashemail@gmail.com>
2025-07-17 15:56:52 -0400

    bruh

---
+++ meowingsim.py
@@ -0,0 +1,23 @@
+def calculadora(a,b,c):
+    if b == "+":
+        print(a+c)
+    elif b == "-":
+        print(a-c)
+    elif b == "*":
+        print(a*c)
+    elif b == "/":
+        print(a/c)
+        if c != 0:
+            print(a/c)
+        else:
+            print("erm actually dividing by zero is not possible so sybau")
+    else: 
+        print("erm you didnt write anything possible for an operation. enter either + - * /")
+
+a = int(input("enter the first number "))
+b = input("Enter the operation ")
+c = int(input("Enter thse second number "))
+calculadora(a, b, c)
+
+print("bruh")
+print("does this work")
